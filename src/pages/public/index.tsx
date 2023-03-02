10125775b3b563c1d8c978d63660f307ee4c5338
NikDoe <nikd0e@yandex.ru>
2023-03-02 17:25:08 +0300

    feat: add basic form for public page

--- src/pages/public/index.tsx
+++ src/pages/public/index.tsx
@@ -1,11 +1,29 @@
-import { FC } from "react";
+import { FC, FormEvent } from "react";
 import { useTitle } from "../../hooks";
 
-import "webli-ui/dist/style.css";
+import { Input } from "../../common";
+import { Button } from "webli-ui";
+
+import styles from "./styles.module.css";
 
 const Public: FC = () => {
 	useTitle("Бронирование билетов | номеров онлайн");
-	return <div>Home page</div>;
+	const publicFormHandle = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+	};
+
+	return (
+		<main className={styles.Main}>
+			<form onSubmit={publicFormHandle} className={styles.Form}>
+				<Input placeholder="откуда" />
+				<Input placeholder="куда" />
+				<Input placeholder="дата" />
+				<Button onClick={() => alert("поиск билетов")}>
+					найти билеты
+				</Button>
+			</form>
+		</main>
+	);
 };
 
 export default Public;
