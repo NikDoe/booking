f9075d4a39835890e9e479ca6dbc1f02c93aecdb
NikDoe <nikd0e@yandex.ru>
2023-04-10 09:42:05 +0300

    fix: indent problem eslint & prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,7 +17,7 @@ module.exports = {
 	},
 	plugins: ["react", "@typescript-eslint"],
 	rules: {
-		indent: ["error", "tab"],
+		// indent: ["error", "tab"],
 		"linebreak-style": ["error", "windows"],
 		quotes: ["error", "double"],
 		semi: ["error", "always"],
