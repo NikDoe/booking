55af8050916e7915b5b123572f824837316532e3
NikDoe <nikd0e@yandex.ru>
2023-07-12 12:57:25 +0300

    feat: add themization
    close #16

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,18 @@ import { Suspense } from "react";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
+import { useTheme } from "./theme/useTheme";
+
 import "./styles/index.scss";
 
 const App = () => {
+  const { theme, toogleTheme } = useTheme();
+
   return (
-    <div className='app dark'>
+    <div className={`app ${theme}`}>
       <Link to='/'>Главная</Link>
       <Link to='/about'>О Нас</Link>
+      <button onClick={toogleTheme}>TOOGLE</button>
       <Suspense fallback={<div>Загрузка...</div>}>
         <Routes>
           <Route path='/' element={<MainPageAsync />} />
