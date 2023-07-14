9805f2279e06682967947f68b1b5f80f571f8322
NikDoe <nikd0e@yandex.ru>
2023-07-14 15:45:22 +0300

    feat: add ```ThemeSwitcher``` shared component
    add svg & file loaders;
    
    close #23

--- src/widgets/Footer/ui/Footer.tsx
+++ src/widgets/Footer/ui/Footer.tsx
@@ -1,6 +1,5 @@
 import { classNames } from "shared/lib/classNames/classNames";
-import { useTheme } from "app/providers/ThemeProvider";
-import { Button } from "shared/ui/Button";
+import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 
 import styles from "./Footer.module.scss";
 
@@ -9,11 +8,9 @@ interface FooterProps {
 }
 
 export const Footer = ({ className }: FooterProps) => {
-  const { toogleTheme } = useTheme();
-
   return (
     <div className={classNames(styles.Footer, {}, [className])}>
-      <Button onClick={toogleTheme}>тема</Button>
+      <ThemeSwitcher />
     </div>
   );
 };
