dd221628701c92d25a0152ab7094e1c20dd6f2b8
NikDoe <nikd0e@yandex.ru>
2023-07-20 11:12:39 +0300

    feat: add ```ErrorBoundary``` provider
    close #31

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import App from './app';
 import ThemeProvider from './app/providers/ThemeProvider/ui/ThemeProvider';
 
 import './shared/config/i18n';
+import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
+        <ErrorBoundary>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </ErrorBoundary>
     </BrowserRouter>
 );
