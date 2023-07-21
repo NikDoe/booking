19e812d03d2be34b9188a48be30e3a7366e938d0
NikDoe <nikd0e@yandex.ru>
2023-07-21 16:59:56 +0300

    refactor: change testing name

--- src/shared/ui/LangSwitcher/ui/LangSwitcher.test.tsx
+++ src/shared/ui/LangSwitcher/ui/LangSwitcher.test.tsx
@@ -2,7 +2,7 @@ import { screen } from '@testing-library/react';
 import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation';
 import { LangSwitcher } from 'shared/ui/LangSwitcher';
 
-describe('Тестирование компонента Button', () => {
+describe('Тестирование компонента LangSwitcher', () => {
     test('присутствует ли элемент в документе', () => {
         renderWithTranslation(<LangSwitcher />);
         expect(screen.getByTestId('lang_switcher')).toBeInTheDocument();
