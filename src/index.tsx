95537bdf757ca471fdbeed51bc40e24d1a228648
NikDoe <nikd0e@yandex.ru>
2023-07-22 19:31:54 +0300

    fix: import for i18n config

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './app';
 import ThemeProvider from './app/providers/ThemeProvider/ui/ThemeProvider';
 
-import './shared/config/i18n';
+import './shared/config/i18n/i18n';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
