5e2e9c74e2b9bdd5abef8f68dd8d908903827ec4
NikDoe <nikd0e@yandex.ru>
2023-07-22 20:20:24 +0300

    refactor: add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,45 @@
 module.exports = {
-  'settings': {
-    'react': {
-      'version': 'detect'
-    }
-  },
-  'env': {
-    'browser': true,
-    'es2021': true,
-    'node': true
-  },
-  'extends': ['eslint:recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:@typescript-eslint/recommended', 'plugin:i18next/recommended', 'plugin:storybook/recommended'],
-  'overrides': [{
-    files: ['**/src/**/*.test.{ts,tsx}'],
-    rules: {
-      'i18next/no-literal-string': 'off'
-    }
-  }],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
-  },
-  'plugins': ['react', '@typescript-eslint', 'i18next'],
-  'rules': {
-    'indent': ['error', 4],
-    'linebreak-style': ['error', 'unix'],
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
-    '@typescript-eslint/no-unused-vars': 'off',
-    'i18next/no-literal-string': ['error', {
-      markupOnly: true
+    'settings': {
+        'react': {
+            'version': 'detect'
+        }
+    },
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended', 
+        'plugin:react/recommended', 
+        'plugin:react/jsx-runtime', 
+        'plugin:@typescript-eslint/recommended', 
+        'plugin:i18next/recommended',
+        'plugin:storybook/recommended'
+        , 
+    ],
+    'overrides': [{
+        files: ['**/src/**/*.test.{ts,tsx}'],
+        rules: {
+            'i18next/no-literal-string': 'off'
+        }
     }],
-    'object-curly-spacing': ['error', 'always']
-  }
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'plugins': ['react', '@typescript-eslint', 'i18next'],
+    'rules': {
+        'max-len': ['error', { 'code': 100, 'ignoreComments': true }],
+        'indent': ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always'],
+        '@typescript-eslint/no-unused-vars': 'off',
+        'i18next/no-literal-string': ['error', {
+            markupOnly: true
+        }],
+        'object-curly-spacing': ['error', 'always']
+    }
 };
