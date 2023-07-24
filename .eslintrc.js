8fbd36224ea0741655c9f15647e34aabb6cb06c9
NikDoe <nikd0e@yandex.ru>
2023-07-24 09:28:49 +0300

    test: setup regression testing w/ ```loki``` close #36

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,12 +18,22 @@ module.exports = {
         'plugin:storybook/recommended'
         , 
     ],
-    'overrides': [{
-        files: ['**/src/**/*.test.{ts,tsx}'],
-        rules: {
-            'i18next/no-literal-string': 'off'
-        }
-    }],
+    'overrides': [
+        {
+            files: ['**/src/**/*.test.{ts,tsx}'],
+            rules: {
+                'i18next/no-literal-string': 'off'
+            }
+        },
+        {
+            files: ['**/*.json'],
+            rules: {
+                'quotes': 'off',
+                'semi': 'off',
+            }
+        },
+
+    ],
     'parser': '@typescript-eslint/parser',
     'parserOptions': {
         'ecmaVersion': 'latest',
