9ae4c9163c3ba1cd605c041d5422eaad6b8201bf
NikDoe <nikd0e@yandex.ru>
2023-07-24 16:17:07 +0300

    feat: add basic ```CI``` pipelines script close #37

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,7 +2,7 @@ import {
     WebpackPluginInstance,
     ProgressPlugin,
     DefinePlugin,
-    HotModuleReplacementPlugin,
+    HotModuleReplacementPlugin
 } from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
@@ -11,24 +11,28 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 import { BuildOptions } from './types/config';
 
-
 export function buildPlugins({ paths, isDev }: BuildOptions): WebpackPluginInstance[] {
-    return [
+    const plugins = [
         new ProgressPlugin(),
         new HtmlWebpackPlugin({
             template: paths.html
         }),
         new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash].css',
+            filename: 'css/[name].[contenthash].css', //куда складывать файлы, в билде
             chunkFilename: 'css/[name].[contenthash].css'
         }),
         new DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new ReactRefreshWebpackPlugin(),
-        new HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        })
     ];
+
+    if(isDev) {
+        plugins.push(new ReactRefreshWebpackPlugin());
+        plugins.push(new HotModuleReplacementPlugin());
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }));
+    }
+
+    return plugins;
 }
